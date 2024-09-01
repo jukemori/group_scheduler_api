73b4837bcf08c2314ea7a9519b4f971d2cdaca73
Jun Ukemori <jukemori@gmail.com>
2024-09-01 10:26:24 +0900

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :api
+  
+  has_and_belongs_to_many :calendars
+  has_many :events, foreign_key: 'user_id'
+
+  validates :color, format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/, message: "must be a valid hex color code" }
+  validates :name, presence: true
+  validates :nickname, presence: true, uniqueness: true
 end

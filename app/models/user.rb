1f3152a355ec6e1ac00262bcfe06c29d43fd0cea
Jun Ukemori <jukemori@gmail.com>
2024-10-11 23:45:38 +0900

    send token from reqeust header

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_create :skip_confirmation!       
   
   has_and_belongs_to_many :calendars
   has_many :events, foreign_key: 'user_id'

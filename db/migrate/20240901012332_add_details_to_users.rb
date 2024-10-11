82e3af0320023b7888b973e3fce0a5bbc00d81a0
Jun Ukemori <jukemori@gmail.com>
2024-10-11 22:26:51 +0900

    remove migration file

--- db/migrate/20240901012332_add_details_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :nickname, :string
-    add_column :users, :color, :string
-  end
-end

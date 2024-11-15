5a80ad11751c8b9c38700435aa5d0e47a601cdaf
Jun Ukemori <jukemori@gmail.com>
2024-11-15 21:41:36 +0900

    add association to invitation model

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,6 +1,8 @@
 class Calendar < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :events, dependent: :destroy
+  has_many :calendar_invitations, dependent: :destroy
+  has_many :invited_users, through: :calendar_invitations, source: :user
 
   validates :name, presence: true
 end

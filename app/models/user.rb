109ba92a9d4f85321245a405c638399e0044cbc4
Jun Ukemori <jukemori@gmail.com>
2024-12-23 11:45:57 +0900

    feat: Add method to handle google auth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   extend Devise::Models
   include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   before_create :skip_confirmation!    
   
   has_one_attached :photo
@@ -29,4 +30,13 @@ class User < ApplicationRecord
   # validates :color, format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/, message: "must be a valid hex color code" }
   validates :name, presence: true
   validates :nickname, presence: true, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.nickname = auth.info.name.parameterize 
+    end
+  end
 end
